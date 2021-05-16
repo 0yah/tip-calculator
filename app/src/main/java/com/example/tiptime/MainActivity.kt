28fdfedc291338482a79a9a4877f10ee2063c092
0yah <highmeme10@gmail.com>
2021-05-16 17:16:30 +0300

    Logic completed and App Icon updated

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+    * lateinit is a promise that the app will initialize a variable before using it*/
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //initialize the binding object which you'll use to access Views in the activity_main.xml layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{calculateTip()}
+    }
+
+    private fun calculateTip() {
+
+        val stringInField = binding.costOfService.text.toString()
+        val cost = stringInField.toDoubleOrNull()
+        if (cost == null) {
+
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.2
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+
+        if(binding.roundUpSwitch.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = formattedTip
+
     }
 }
